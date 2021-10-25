5a0d9dc14008ce3de1dff97f9581fc6dfa265e27
Shivansh Mehrotra <shivansh.sm1@gmail.com>
2021-10-26 01:17:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
+const goodbooks = {
+  javascript: [
+    { name: "Eloquent javascript", rating: "4.5/5" },
+    { name: "You Dont know JS", rating: "4/5" },
+  ],
+  Python: [
+    { name: "Python Crash Course", rating: "3/5" },
+    { name: "Fluent Python", rating: "3.5/5" },
+  ],
+  Fiction: [
+    { name: "Harry Potter Series", rating: "4/5" },
+    { name: "Marvel : Ironman", rating: "4/5" },
+  ],
+};
 function App() {
+  const [getGenre, setGenreHandler] = useState("Fiction");
+  function clickHandler(genre) {
+    setGenreHandler(genre);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>📘bestreads</h1>
+      <p>Checkout books available in our store.Select a genre to look</p>
+      <div>
+        {Object.keys(goodbooks).map((genre) => (
+          <button
+            onClick={() => clickHandler(genre)}
+            key={genre}
+            className="btn"
+          >
+            {genre}
+          </button>
+        ))}
+      </div>
+      <hr />
+      <ul>
+        {goodbooks[getGenre].map((book) => (
+          <li>
+            <b>Name : </b>
+            {book.name}, <b>rating</b> : {book.rating}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
